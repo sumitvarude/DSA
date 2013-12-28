288dc2ce143c1722ec2b72c067d7242bba2758d8
unknown <sumitvar@INsumitvar.corporate.thoughtworks.com>
2013-12-28 15:19:06 +0530

    bubble compatible with float

--- bubble_sort/bubbleSortTest.c
+++ bubble_sort/bubbleSortTest.c
@@ -5,6 +5,12 @@
 int compareInts(void *element1,void *element2){
 	return (*(int*)element1)-(*(int*)element2);
 }
+int compareDoubles(void *element1,void *element2){
+	return (*(double*)element1)-(*(double*)element2);
+}
+int compareChars(void *element1,void *element2){
+	return (*(char*)element1)-(*(char*)element2);
+}
 int isSorted(void* array,int noOfElements,int typeSize,Compare compare){
 	int index,result;
 	for(index = 0;index < noOfElements;index++){
@@ -20,21 +26,22 @@ void test_gives_one_if_array_is_sorted(){
 	ASSERT(1 == isSorted(data, 2,sizeof(int), compareInts));	
 }
 void test_gives_zero_if_array_is_not_sorted(){
-	void *sortedArray;
+	void *sortedArray; 
 	int data[] = {3,2,1};
 	ASSERT(0 == isSorted(data, 2,sizeof(int), compareInts));	
 }
-void test_sorts_array(){
+void test_sorts_array_with_only_two_elements(){
 	int data[] = {2,1};
 	performSort(data, 2,sizeof(int),compareInts);
 	ASSERT(0 == isSorted(data, 2,sizeof(int), compareInts));	
 }
-void test_sorts_array1(){
+void test_sorts_array_with_multiple_elements(){
 	int data[] = {10,4,9,9,2,3,5,1};
 	performSort(data, 8,sizeof(int),compareInts);
 	ASSERT(0 == isSorted(data, 8,sizeof(int), compareInts));	
 }
-void test_sorts_array2(){
-	int data[] = {2};
-	performSort(data, 1,sizeof(int),compareInts);
+void test_sorts_array_of_double_elements(){
+	double data[] = {3.0,2.0,1.0};
+	performSort(data, 3,sizeof(double), compareDoubles);
+	ASSERT(0 == isSorted(data, 3,sizeof(double), compareDoubles));
 }
