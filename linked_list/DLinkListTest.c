b5249b2633a029861242366fe25501ff2bcb7f97
unknown <sumitvar@INsumitvar.corporate.thoughtworks.com>
2013-12-30 15:03:19 +0530

    linked list with changed testa

--- linked_list/DLinkListTest.c
+++ linked_list/DLinkListTest.c
@@ -4,86 +4,77 @@
 #include <stdio.h>
 //create setup, tearDown, fixtureSetup, fixtureTearDown methods if needed
 
-void test_1_create_a_linked_list(){
-	List* list = malloc(sizeof(Node));
-	list = createList();
-	ASSERT(list->head == NULL);
-	ASSERT(0 == list->noOfElements);
-}
-
-void test_2_insert_a_node_of___INT___type_AT_START(){
+void test_1_insert_a_node_of___INT___type_AT_START(){
 	int result;
 	List* list = createList();
 	int element = 3;
-	result = insertNode(list, 0,&element);
-	ASSERT(result == 1);
-	ASSERT(*(int*)list->head->data == 3);
-	ASSERT(list->noOfElements == 1);
+	insertNode(list, 0,&element);
+	ASSERT(removeElement(list, 0));
 }
 
-void test_3_insert_a_node_of___INT___type_AT_LAST(){
+void test_2_insert_a_node_of___INT___type_AT_LAST(){
 	int result;
 	List* list = createList();
 	int element = 3;
 	insertNode(list, 0,&element);
 	result = insertNode(list,1,&element);
 	ASSERT(result == 1);
-	ASSERT(list->noOfElements == 2);
+	ASSERT(removeElement(list, 0));
+	ASSERT(removeElement(list, 1));
 }
 
-void test_4_insert_a_node_of___FLOAT___type_AT_START(){
+void test_3_insert_a_node_of___FLOAT___type_AT_START(){
 	int result;
 	List* list = createList();
 	float element = 3.3;
 	result = insertNode(list,0,&element);
 	ASSERT(result == 1);
-	ASSERT(list->head->data == &element);
-	ASSERT(list->noOfElements == 1);
+	ASSERT(removeElement(list, 0));
 }
 
-void test_5_insert_a_node_of___FLOAT___type_AT_LAST(){
+void test_4_insert_a_node_of___FLOAT___type_AT_LAST(){
 	int result;
 	List* list = createList();
 	float element = 3.3;
 	result = insertNode(list,0,&element);
 	result = insertNode(list,1,&element);
 	ASSERT(result == 1);
-	ASSERT(list->noOfElements == 2);
+	ASSERT(removeElement(list, 0));
+	ASSERT(removeElement(list, 1));
 }
 
-void test_6_insert_a_node_of___DOUBLE___type_AT_START(){
+void test_5_insert_a_node_of___DOUBLE___type_AT_START(){
 	int result;
 	List* list = createList();
 	double element = 3.6;
 	result = insertNode(list, 0,&element);
 	ASSERT(result == 1);
-	ASSERT(list->head->data == &element);
-	ASSERT(list->noOfElements == 1);
+	ASSERT(removeElement(list, 0));
 }
 
-void test_7_insert_a_node_of___DOUBLE___type_AT_LAST(){
+void test_6_insert_a_node_of___DOUBLE___type_AT_LAST(){
 	int result;
 	List* list = createList();
 	double element = 3.6;
 	result = insertNode(list, 0,&element);
 	result = insertNode(list, 1,&element);
 	ASSERT(result == 1);
-	ASSERT(list->noOfElements == 2);
+	ASSERT(removeElement(list, 0));
+	ASSERT(removeElement(list, 1));
 }
 
-void test_8_insert_a_node_of___CHAR___type_AT_START(){
+void test_7_insert_a_node_of___CHAR___type_AT_START(){
 	int result;
 	List* list = createList();
 	char *element = malloc(sizeof(char));
 	*element = 'a';
 	result = insertNode(list, 0,&element);
 	ASSERT(result == 1);
-	ASSERT(list->head->data == &element);
-	ASSERT(list->noOfElements == 1);
+	ASSERT(removeElement(list, 0));
 	free(element);
 }
 
-void test_9_inserts_a_node_of___INT___type_at_THIRD_position(){
+void test_8_inserts_a_node_of___INT___type_at_THIRD_position(){
 	int result;
 	Node* thirdNode = calloc(1,sizeof(Node)); 
 	List* list = createList();
@@ -97,7 +88,7 @@ void test_9_inserts_a_node_of___INT___type_at_THIRD_position(){
 	ASSERT(3==*(int*)thirdNode->data);
 }
 
-void test_10_inserts_a_node_of___FLOAT___type_at_THIRD_position(){
+void test_9_inserts_a_node_of___FLOAT___type_at_THIRD_position(){
 	int result;
 	Node* thirdNode = calloc(1,sizeof(Node)); 
 	List* list = createList();
@@ -110,7 +101,7 @@ void test_10_inserts_a_node_of___FLOAT___type_at_THIRD_position(){
 	ASSERT(result == 1);
 	ASSERT(3.0==*(double*)thirdNode->data);
 }
-void test_11_inserts_a_node_of___CHAR___type_at_THIRD_position(){
+void test_10_inserts_a_node_of___CHAR___type_at_THIRD_position(){
 	int result;
 	Node* thirdNode = calloc(1,sizeof(Node)); 
 	List* list = createList();
@@ -124,13 +115,13 @@ void test_11_inserts_a_node_of___CHAR___type_at_THIRD_position(){
 	ASSERT('c'==*(char*)thirdNode->data);
 }
 
-void test_12_gives_length_of_given_list(){
+void test_11_gives_length_of_given_list(){
 	List* list = createList();
 	list->noOfElements = 10;
 	ASSERT(10 == getLength(list));
 }
  
-void test_13_removes_a_node_of___CHAR___type_at_THIRD_position(){
+void test_12_removes_a_node_of___CHAR___type_at_THIRD_position(){
 	List* list = createList();
 	Node* node = calloc(1,sizeof(Node));
 	int one = 1,two = 2,three = 3;
@@ -144,7 +135,7 @@ void test_13_removes_a_node_of___CHAR___type_at_THIRD_position(){
 int compareIntData(void* element1 , void* element2){
         return *(int*)element1 - *(int*)element2;
 }
-void test_search_data_in_given_list_return_index(){
+void test_13_search_data_in_given_list_return_index(){
         List* list = createList();
         int data[] = {100,200,300,400};
 
@@ -154,19 +145,17 @@ void test_search_data_in_given_list_return_index(){
 
         ASSERT(1 == search(list, &data[1], compareIntData));
 }
-void test_search_data_in_empty_list_return_minus_one(){
+void test_14_search_data_in_empty_list_return_minus_one(){
         List* list = createList();
         int data = 10;
         ASSERT(-1 == search(list, &data, compareIntData));
 }
-void test_search_gives_minus_one_if_data_not_found(){
+void test_15_search_gives_minus_one_if_data_not_found(){
         List* list = createList();
         int data[] = {100,200,300,400};
         int element = 10;
-
         insertNode(list,0,&data[0]);
         insertNode(list,1,&data[1]);
         insertNode(list,2,&data[2]);
-
         ASSERT(-1 == search(list, &element, compareIntData));
 }
